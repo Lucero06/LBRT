4a7066133d7cbb1e67902e0018c41db6a2f5a708
L. Monse S.P <lmonserrat.sanchez@gmail.com>
2022-07-20 11:31:21 -0500

    Cambios menores parametros sobrantes funcion find n blocks stop

--- lbrt/tarea/tasks.py
+++ lbrt/tarea/tasks.py
@@ -120,7 +120,7 @@ def iniciar_orden(channel_name, limit, pool, algoritmo, amount):
         return order_id
 
 @shared_task
-def loop_find_n_blocks(order_id,channel_name,miner=None):
+def loop_find_n_blocks(order_id,channel_name,miner):
     print('Tarea find n blocks iniciada... ')
     async_to_sync(channel_layer.group_send)("tarea", {"type": "tarea.message", 
                                         "message": 
@@ -498,14 +498,14 @@ def loop_update_limit(channel_name, pool, algoritmo,limit_1,limit_2, amount, tim
 
 @app.task
 def chain_find_blocks_stop(channel_name, pool_id, pool_algorithm, miner,amount, limit):
-    res = (iniciar_orden.s(str(channel_name), limit,pool_id,pool_algorithm, amount) | loop_find_n_blocks_stop.s(str(channel_name),miner, limit, pool_algorithm) | detener_orden.s(str(channel_name))  )()
+    res = (iniciar_orden.s(str(channel_name), limit,pool_id,pool_algorithm, amount) | loop_find_n_blocks_stop.s(str(channel_name),miner) | detener_orden.s(str(channel_name))  )()
     #res =(test_task.s() | test_task_2.s() | test_task_3.s())()
     return res
 #
 
 
 @shared_task
-def loop_find_n_blocks_stop(order_id,channel_name,miner=None):
+def loop_find_n_blocks_stop(order_id,channel_name,miner):
     print('Tarea find n blocks iniciada... ')
     async_to_sync(channel_layer.group_send)("tarea", {"type": "tarea.message", 
                                         "message": 
